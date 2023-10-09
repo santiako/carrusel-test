ba98f6f9c9e30d6f7c4c2ef3663f7e41b74e2bfc
Santiago Insúa <52498688+santiako@users.noreply.github.com>
2023-10-09 16:43:25 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// @app
+import Carrusel from './components/Carrusel';
+
+function App() {
+  return (
+    <div className="bg-sky-600">
+      <Carrusel />
+    </div>
+  );
+}
+
+export default App;
